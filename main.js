e0cf1ecf2c2ce60914c7b53a48f55fc61708d2e2
rushiaismywaifu <82970140+rushiaismywaifu@users.noreply.github.com>
2023-11-06 01:06:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const cursor = document.querySelector('.custom-cursor');
-    const particles = [];
+    const cursor = document.createElement('div');
+    cursor.className = 'custom-cursor';
+    document.body.appendChild(cursor);
 
     document.addEventListener('mousemove', (e) => {
         cursor.style.top = e.pageY + 'px';
         cursor.style.left = e.pageX + 'px';
-
-        createParticle(e.pageX, e.pageY);
     });
-
-    function createParticle(x, y) {
-        const particle = document.createElement('div');
-        particle.className = 'particle';
-        document.body.appendChild(particle);
-
-        particle.style.top = y + 'px';
-        particle.style.left = x + 'px';
-
-        particles.push(particle);
-
-        // 移除粒子
-        setTimeout(() => {
-            particle.remove();
-            particles.shift();
-        }, 1000);
-    }
 });
